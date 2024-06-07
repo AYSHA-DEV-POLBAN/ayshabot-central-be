b4f9d0ddfb4d546675d7bbdb5282e8c917e192c0
brada1604 <bagusnugroho1604@gmail.com>
2024-06-07 14:21:25 +0700

    <FIX> Migration users : update field

--- migrations/20230521040546-users.js
+++ migrations/20230521040546-users.js
@@ -1,49 +1,59 @@
-'use strict';
+"use strict";
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Users', {
-      id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      username: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      password: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      email: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      verify_email: {
-        type: Sequelize.STRING,
-        allowNull: true,
-      },
-      createdAt: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-      updatedAt: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-    }, {
-      schema: 'public', // Menentukan skema tabel
-    });
-  },
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable(
+			"users",
+			{
+				id: {
+					type: Sequelize.INTEGER,
+					allowNull: false,
+					autoIncrement: true,
+					primaryKey: true,
+				},
+				role_id: {
+					type: Sequelize.INTEGER,
+					allowNull: false,
+				},
+				name: {
+					type: Sequelize.STRING,
+					allowNull: false,
+				},
+				email: {
+					type: Sequelize.STRING,
+					allowNull: false,
+				},
+				verify_email: {
+					type: Sequelize.INTEGER,
+					allowNull: true,
+				},
+				password: {
+					type: Sequelize.TEXT,
+					allowNull: false,
+				},
+				status: {
+					type: Sequelize.INTEGER,
+					allowNull: false,
+				},
+				created_at: {
+					type: Sequelize.DATE,
+					allowNull: false,
+					defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+				},
+				updated_at: {
+					type: Sequelize.DATE,
+					allowNull: false,
+					defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+				},
+			},
+			{
+				schema: "public", // Menentukan skema tabel
+			}
+		);
+	},
 
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Users', {
-      schema: 'public', // Menentukan skema tabel
-    });
-  }
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable("users", {
+			schema: "public", // Menentukan skema tabel
+		});
+	},
 };
