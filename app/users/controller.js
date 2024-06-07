dbf9f1a9db7442b0b3fa5430437d80d8681fa455
brada1604 <bagusnugroho1604@gmail.com>
2024-06-07 21:07:12 +0700

    <FEAT> User : activate deactive account, edit account

--- app/users/controller.js
+++ app/users/controller.js
@@ -12,7 +12,7 @@ module.exports = {
 	},
 	add_operator: async (req, res) => {
 		try {
-			const { role_id, name, email, password,status } = req.body;
+			const { role_id, name, email, password, status } = req.body;
 
 			// Hash the password
 			const hashedPassword = await bcrypt.hash(password, 10);
@@ -24,29 +24,167 @@ module.exports = {
 				password: hashedPassword,
 				status,
 			});
-			res.status(200).json({ data: newUser, status:"User berhasil dibuat" });
+			res.status(200).json({ data: newUser, status: "User berhasil dibuat" });
 		} catch (err) {
 			res.status(500).json({ message: err.message || "internal server error" });
 		}
 	},
 	actionDelete: async (req, res) => {
-        try {
-            const { id } = req.params;
-			
-			if (id != 1) {
-	            await User.destroy({ where:{id: id} });
-	          
-	            res.status(200).json({
-	                message: "Berhasil Hapus User"
-	            })
-			}   
-			else {
+		try {
+			const { id } = req.params;
+
+			const user = await User.findOne({ where: { id: id } });
+			if (user.role_id != 1) {
+				await User.destroy({ where: { id: id } });
+
 				res.status(200).json({
-	                message: "Superadmin Cannot Delete"
-	            })
+					message: "Berhasil Hapus User",
+				});
+			} else {
+				res.status(200).json({
+					message: "Superadmin Cannot Delete",
+				});
 			}
+		} catch (err) {
+			res.status(500).json({ message: err.message || "internal server error" });
+		}
+	},
+	actionActivateAccountOperator: async (req, res) => {
+		try {
+			const { id } = req.params;
+
+			const user = await User.findOne({ where: { id: id } });
+			if (user.role_id != 1) {
+				await User.update({ status: 1 }, { where: { id: id } })
+
+				res.status(200).json({
+					message: "Berhasil Aktivasi User",
+				});
+			} else {
+				res.status(200).json({
+					message: "Superadmin Cannot Updated via app",
+				});
+			}
+		} catch (err) {
+			res.status(500).json({ message: err.message || "internal server error" });
+		}
+	},
+	actionDeactivateAccountOperator: async (req, res) => {
+		try {
+			const { id } = req.params;
+
+			const user = await User.findOne({ where: { id: id } });
+			if (user.role_id != 1) {
+				await User.update({ status: 0 }, { where: { id: id } })
+
+				res.status(200).json({
+					message: "Berhasil Deaktivasi User",
+				});
+			} else {
+				res.status(200).json({
+					message: "Superadmin Cannot Updated via app",
+				});
+			}
+		} catch (err) {
+			res.status(500).json({ message: err.message || "internal server error" });
+		}
+	},
+	actionActivateEmailOperator: async (req, res) => {
+		try {
+			const { id } = req.params;
+
+			const user = await User.findOne({ where: { id: id } });
+			if (user.role_id != 1) {
+				await User.update({ verify_email: 1 }, { where: { id: id } })
+
+				res.status(200).json({
+					message: "Berhasil Aktivasi Email User",
+				});
+			} else {
+				res.status(200).json({
+					message: "Superadmin Cannot Updated via app",
+				});
+			}
+		} catch (err) {
+			res.status(500).json({ message: err.message || "internal server error" });
+		}
+	},
+	actionDeactivateEmailOperator: async (req, res) => {
+		try {
+			const { id } = req.params;
+
+			const user = await User.findOne({ where: { id: id } });
+			if (user.role_id != 1) {
+				await User.update({ verify_email: 0 }, { where: { id: id } })
+
+				res.status(200).json({
+					message: "Berhasil Deaktivasi Email User",
+				});
+			} else {
+				res.status(200).json({
+					message: "Superadmin Cannot Updated via app",
+				});
+			}
+		} catch (err) {
+			res.status(500).json({ message: err.message || "internal server error" });
+		}
+	},
+	actionEdit: async (req, res, next) => {
+        const { id } = req.params;
+        try {
+            const { name = "", email = "" } = req.body;
+            const payload = {};
+            if (name.length) payload.name = name;
+            
+            status = 0;
+            verify_email = 0;
+
+            // Memastikan bahwa user dengan ID tersebut ada
+            let user = await User.findOne({ where: { id: id } });
+            
+            // cek apakah email yg di inputkan sama atau tidak dengan email sebelumnya, jika tidak sama maka indikasi email di update
+            if (user.email != req.body.email) {
+            	if (email.length) payload.email = email;
+            	payload.status = status;
+            	payload.verify_email = verify_email;
+            }
+
+            if (!user) {
+                return res.status(404).json({
+                    error: 1,
+                    message: "User not found",
+                });
+            }
+
+            // Mengupdate data user
+            await User.update(payload, { where: { id: id } })
+                .then((updatedRows) => {
+                    console.log(`${updatedRows} rows updated successfully.`);
+                    res.status(200).json({
+                        data: {
+                            id: id,
+                            name: payload.name || user.name,  // Tampilkan nama yang baru atau lama jika tidak diupdate
+                            email: payload.email || user.email // Tampilkan email yang baru atau lama jika tidak diupdate
+                        },
+						message: "Update User Successfully"
+                    });
+                })
+                .catch((err) => {
+                    console.error('Error:', err);
+                    res.status(422).json({
+                        error: 1,
+                        message: err.message,
+                        fields: err.errors
+                    });
+                });
+
         } catch (err) {
-            res.status(500).json({message: err.message || 'internal server error'})
+            console.error('Error:', err);
+            res.status(500).json({
+                error: 1,
+                message: 'Internal server error',
+            });
         }
-    },
+    }
+	
 };
--- app/users/router.js
+++ app/users/router.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
-const { users, add_operator, actionDelete } = require('./controller');
+const { 
+	users, 
+	add_operator, 
+	actionDelete, 
+	actionActivateAccountOperator, 
+	actionDeactivateAccountOperator, 
+	actionActivateEmailOperator, 
+	actionDeactivateEmailOperator,
+	actionEdit } = require('./controller');
 const { isLoginAdmin, isLoginUser } = require('../middleware/auth');
 
 router.get('/', isLoginAdmin, users);
 router.post('/add_operator', isLoginAdmin, add_operator);
 router.delete('/delete/:id', isLoginAdmin, actionDelete);
+router.put('/activate_account_operator/:id', isLoginAdmin, actionActivateAccountOperator);
+router.put('/deactivate_account_operator/:id', isLoginAdmin, actionDeactivateAccountOperator);
+router.put('/activate_email_operator/:id', isLoginAdmin, actionActivateEmailOperator);
+router.put('/deactivate_email_operator/:id', isLoginAdmin, actionDeactivateEmailOperator);
+router.put('/edit/:id', isLoginAdmin, actionEdit);
 module.exports = router;
