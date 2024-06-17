8da8f638c39377f491c02baaec94118e29a413d0
brada1604 <bagusnugroho1604@gmail.com>
2024-06-18 01:21:30 +0700

    <FEAT> Information : edit with file

--- app/information/controller.js
+++ app/information/controller.js
@@ -161,6 +161,122 @@ module.exports = {
             });
         }
     },
+    actionEditUploadFile: async (req, res, next) => {
+        try {
+            const { id } = req.params;
+            const { category_information_id ="", title_information ="", description_information ="" } = req.body;
+
+            const payload = {};
+
+            // Memastikan bahwa information dengan ID tersebut ada
+            let information = await Information.findOne({ where: { id: id } });
+
+            if (!information) {
+                return res.status(404).json({ error: 1, message: "Information not found", });
+            }
+
+            if (information.category_information_id != category_information_id) {
+                if (category_information_id.length) payload.category_information_id = category_information_id;
+            }
+
+            if (information.title_information != title_information) {
+                if (title_information.length) payload.title_information = title_information;
+            }
+
+            if (information.description_information != description_information) {
+                if (description_information.length) payload.description_information = description_information;
+            }
+
+            if (req.file) {
+                let temporary_path = req.file.path;
+                let original_extension = req.file.originalname.split('.')[req.file.originalname.split('.').length - 1];
+                let filename = req.file.filename + '.' + original_extension;
+                let target_path = path.resolve(config.rootPath, `public/uploads/document_information/${filename}`);
+                const base_url = `${req.protocol}://${req.get('host')}`;
+                let file_path_information = base_url + "/uploads/document_information/" + filename;
+
+                if (information.file_path_information != file_path_information) {
+                    if (file_path_information.length) payload.file_path_information = file_path_information;
+                }
+
+                const path_old = information.file_path_information;
+
+                // Split the string by "/" and get the last element
+                const parts = path_old.split('/');
+                const filename_old = parts[parts.length - 1];
+
+                let old_document = `${config.rootPath}/public/uploads/document_information/${filename_old}`;
+                if (fs.existsSync(old_document)) {
+                    fs.unlinkSync(old_document)
+                }
+
+
+                const src = fs.createReadStream(temporary_path);
+                const destination = fs.createWriteStream(target_path);
+
+                src.pipe(destination)
+                src.on('end', async () => {
+                    try {
+                        // Mengupdate data information
+                        await Information.update(payload, { where: { id: id } })
+                            .then((updatedRows) => {
+                                console.log(`${updatedRows} rows updated successfully.`);
+                                res.status(200).json({
+                                    data: {
+                                        id: id,
+                                        category_information_id: payload.category_information_id || information.category_information_id,
+                                        title_information: payload.title_information || information.title_information,
+                                        description_information: payload.description_information || information.description_information,
+                                        file_path_information: payload.file_path_information || information.file_path_information,
+                                    },
+                                    message: "Update Information Successfully"
+                                });
+                            })
+                            .catch((err) => {
+                                console.error('Error:', err);
+                                res.status(422).json({
+                                    error: 1,
+                                    message: err.message,
+                                    fields: err.errors
+                                });
+                            });
+                    } catch (err) {
+                        if (err && err.name === 'SequelizeValidationError') {
+                            return res.status(422).json({ error: 1, message: err.message, fields: err.errors })
+                        }
+                        next(err);
+                    }
+                });
+            } else {
+                // res.status(500).json({ message: 'Document is required!' })
+                // Mengupdate data information
+                await Information.update(payload, { where: { id: id } })
+                    .then((updatedRows) => {
+                        console.log(`${updatedRows} rows updated successfully.`);
+                        res.status(200).json({
+                            data: {
+                                id: id,
+                                category_information_id: payload.category_information_id || information.category_information_id,
+                                title_information: payload.title_information || information.title_information,
+                                description_information: payload.description_information || information.description_information,
+                                file_path_information: payload.file_path_information || information.file_path_information,
+                            },
+                            message: "Update Information Successfully"
+                        });
+                    })
+                    .catch((err) => {
+                        console.error('Error:', err);
+                        res.status(422).json({
+                            error: 1,
+                            message: err.message,
+                            fields: err.errors
+                        });
+                    });
+            }
+        } catch (err) {
+            res.status(500).json({ message: err.message || 'internal server error' })
+        }
+    },
     actionActivateInformation: async (req, res) => {
 		try {
 			const { id } = req.params;
--- app/information/router.js
+++ app/information/router.js
@@ -10,6 +10,7 @@ const {
 	actionCreatedUploadFile,
 	actionDelete,
 	actionEdit,
+	actionEditUploadFile,
 	actionActivateInformation,
 	actionDeactivateInformation } = require('./controller');
 const { isLoginAdmin, isLoginUser } = require('../middleware/auth');
@@ -20,6 +21,7 @@ router.post('/create', isLoginUser, actionCreated);
 router.post('/create_with_upload_file', multer({ dest: os.tmpdir() }).single('document'), actionCreatedUploadFile);
 router.delete('/delete/:id', isLoginUser, actionDelete);
 router.put('/edit/:id', isLoginUser, actionEdit);
+router.put('/edit_with_upload_file/:id', multer({ dest: os.tmpdir() }).single('document'), actionEditUploadFile);
 router.put('/activate_information/:id', isLoginUser, actionActivateInformation);
 router.put('/deactivate_information/:id', isLoginUser, actionDeactivateInformation);
 module.exports = router;
