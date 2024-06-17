80c560a7a00f23447041d84ae7990e1a56d1018b
brada1604 <bagusnugroho1604@gmail.com>
2024-06-18 01:37:50 +0700

    <DEL> FOLDER : Delete unused File

--- app/product/router.js
+++ app/product/router.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var router = express.Router();
-const { index,actionCreated,actionDelete,actionEdit} = require('./controller');
-const { isLoginUser } = require('../middleware/auth');
+// var express = require('express');
+// var router = express.Router();
+// const { index,actionCreated,actionDelete,actionEdit} = require('./controller');
+// const { isLoginUser } = require('../middleware/auth');
 
-router.get('/', index);
-router.post('/create', actionCreated);
-router.delete('/delete/:id', actionDelete);
-router.put('/edit/:id', actionEdit);
-module.exports = router;
+// router.get('/', index);
+// router.post('/create', actionCreated);
+// router.delete('/delete/:id', actionDelete);
+// router.put('/edit/:id', actionEdit);
+// module.exports = router;
--- app/productGalery/model.js
+++ app/productGalery/model.js
@@ -1,33 +1,33 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../../database/sequelize');
+// const { DataTypes } = require('sequelize');
+// const sequelize = require('../../database/sequelize');
 
-const ProductGallery = sequelize.define('ProductGallery', {
-  ProductId:{
-    type: DataTypes.INTEGER,
-    allowNull: true,
+// const ProductGallery = sequelize.define('ProductGallery', {
+//   ProductId:{
+//     type: DataTypes.INTEGER,
+//     allowNull: true,
     
-  },
-  GalleryId:{
-    type: DataTypes.INTEGER,
-    allowNull: true,
-  },
+//   },
+//   GalleryId:{
+//     type: DataTypes.INTEGER,
+//     allowNull: true,
+//   },
   
-  createdAt: {
-    type: DataTypes.DATE,
-    allowNull: false,
-  },
-  updatedAt: {
-    type: DataTypes.DATE,
-    allowNull: false,
-  },
-  deletedAt: {
-    type: DataTypes.DATE,
-    allowNull: false,
-  },
+//   createdAt: {
+//     type: DataTypes.DATE,
+//     allowNull: false,
+//   },
+//   updatedAt: {
+//     type: DataTypes.DATE,
+//     allowNull: false,
+//   },
+//   deletedAt: {
+//     type: DataTypes.DATE,
+//     allowNull: false,
+//   },
   
-}, {
-  tableName: 'ProductGallery',
-  schema: 'public',
-});
+// }, {
+//   tableName: 'ProductGallery',
+//   schema: 'public',
+// });
 
-module.exports = ProductGallery;
+// module.exports = ProductGallery;
