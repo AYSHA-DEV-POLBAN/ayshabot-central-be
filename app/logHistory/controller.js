f749a7f65f3beb7f11adfbd7e5f79944305d7491
brada1604 <bagusnugroho1604@gmail.com>
2024-06-17 07:39:39 +0700

    <FEAT> Log history : get by id

--- app/logHistory/controller.js
+++ app/logHistory/controller.js
@@ -9,18 +9,6 @@ module.exports = {
 			res.status(500).json({ message: err.message || "internal server error" });
 		}
 	},
-	actionCreated: async (req, res) => {
-		try {
-			const { user_id, client_id, table_name, action_name, action_data, log_description} = req.body;
-
-			const newLogHistory = await LogHistory.create({
-				user_id, client_id, table_name, action_name, action_data, log_description
-			});
-			res.status(200).json({ data: newLogHistory, status: "Log History berhasil dibuat" });
-		} catch (err) {
-			res.status(500).json({ message: err.message || "internal server error" });
-		}
-	},
 	logHistoryCreated: async (user_id, client_id, table_name, action_name, action_data, log_description) => {
 		try {
 			// const { user_id, client_id, table_name, action_name, action_data, log_description} = req.body;
@@ -39,6 +27,50 @@ module.exports = {
 			console.log(err.message || "internal server error");
 			console.log("=======================================");
 			// res.status(500).json({ message: err.message || "internal server error" });
+		}
+	},
+	getLogHistoryById: async (req, res) => {
+		try {
+			const { id } = req.params;
+			const log_history = await LogHistory.findOne({ where: { id: id } });
+
+			const payload = {};
+
+			payload.user_id = req.user.id;
+			payload.client_id = null;
+			payload.table_name = LogHistory.getTableName().tableName;
+			payload.action_name = "GET DATA";
+			payload.action_data = JSON.stringify(log_history, null, 4) + " --> " + req.user.email;
+			payload.log_description = "LogHistory.findOne({ where: { id: id } })";
+
+			console.log(payload);
+			const logging = await LogHistory.create(payload);
+			res.status(200).json({ data: log_history });
+		} catch (err) {
+			const payload = {};
+
+			payload.user_id = req.user.id;
+			payload.client_id = null;
+			payload.table_name = LogHistory.getTableName().tableName;
+			payload.action_name = "ERROR";
+			payload.action_data = "-";
+			payload.log_description = err.message || "internal server error";
+
+			console.log(payload);
+			const logging = await LogHistory.create(payload);
+			res.status(500).json({ message: err.message || "internal server error" });
+		}
+	},
+	actionCreated: async (req, res) => {
+		try {
+			const { user_id, client_id, table_name, action_name, action_data, log_description} = req.body;
+
+			const newLogHistory = await LogHistory.create({
+				user_id, client_id, table_name, action_name, action_data, log_description
+			});
+			res.status(200).json({ data: newLogHistory, status: "Log History berhasil dibuat" });
+		} catch (err) {
+			res.status(500).json({ message: err.message || "internal server error" });
 		}
 	},
 	actionDelete: async (req, res) => {
--- app/logHistory/router.js
+++ app/logHistory/router.js
@@ -2,11 +2,13 @@ var express = require('express');
 var router = express.Router();
 const { 
 	index,
+	getLogHistoryById,
 	actionCreated,
 	actionDelete } = require('./controller');
 const { isLoginAdmin, isLoginUser } = require('../middleware/auth');
 
 router.get('/', isLoginAdmin, index);
+router.get('/get_log_history_by_id/:id', isLoginUser, getLogHistoryById);
 router.post('/create', actionCreated);
 router.delete('/delete/:id', isLoginAdmin, actionDelete);
 module.exports = router;
