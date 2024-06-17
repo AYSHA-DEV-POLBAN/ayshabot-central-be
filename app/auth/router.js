bb862869c80546f155f9c2a129abc0f35f55ffd9
brada1604 <bagusnugroho1604@gmail.com>
2024-06-18 02:22:44 +0700

    <FIX> AUTH : Router Clean Code

--- app/auth/router.js
+++ app/auth/router.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 const { signup, signin } = require('./controller');
-/* GET home page. */
 
 router.post('/signup', signup);
 router.post('/signin', signin);
